6a1c436df6abe799cb8bcff1ed93cf079eac4728
kushagrasavya <kushagrasavya.choudhary@gmail.com>
2026-05-06 10:24:56 +0530

    Feat: Integrated Google Analytics 4

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
+import ReactGA from "react-ga4";
+
+// Initialize Google Analytics with your Measurement ID
+ReactGA.initialize("G-CFHCVS6TJV");
+
+// Send the initial pageview as soon as the app loads
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
